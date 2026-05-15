975a7f6f13478a27b9ba05a298391ca2b014a14f
noro <92811588+noroblooom@users.noreply.github.com>
2026-05-15 18:07:45 +0100

    Add: sort-an-array - submission-26

---
+++ Data Structures & Algorithms/sort-an-array/submission-26.py
@@ -0,0 +1,45 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def partition(arr, l, r):
+            m = (l + r) >> 1
+            arr[m], arr[l + 1] = arr[l + 1], arr[m]
+
+            if arr[l] > arr[r]:
+                arr[l], arr[r] = arr[r], arr[l]
+            if arr[l + 1] > arr[r]:
+                arr[l + 1], arr[r] = arr[r], arr[l + 1]
+            if arr[l] > arr[l + 1]:
+                arr[l], arr[l + 1] = arr[l + 1], arr[l]
+            
+            pivot = arr[l + 1]
+            i = l + 1
+            j = r
+
+            while True:
+                while True:
+                    i += 1
+                    if pivot <= arr[i]:
+                        break
+                while True:
+                    j -= 1
+                    if pivot >= arr[j]:
+                        break
+                if i > j:
+                    break
+                arr[i], arr[j] = arr[j], arr[i]
+            
+            arr[j], arr[l + 1] = arr[l + 1], arr[j]
+            return j
+
+        def quickSort(arr, l, r):
+            if l + 1 >= r:
+                if r == l + 1 and arr[l] > arr[r]:
+                    arr[l], arr[r] = arr[r], arr[l]
+                return
+            
+            j = partition(arr, l, r)
+            quickSort(arr, l, j - 1)
+            quickSort(arr, j + 1, r)
+        
+        quickSort(nums, 0, len(nums) - 1)
+        return nums
